3d90419048bd4674e86ae1f359055da05747204b
ls-age CI <ci@ls-age.com>
2019-10-27 17:50:59 +0000

    style: Fix broken anchors [ci skip]

--- client/client.5d281228.js
+++
@@ -1,1 +0,0 @@
-import"./SEO.d01c8e11.js";import{s as e}from"./app.57d3c330.js";e({target:document.querySelector("#sapper")});
---
+++ client/legacy/client.f9fa43cc.js
@@ -0,0 +1,1 @@
+import"./SEO.ad012e6e.js";import{s as e}from"./app.177c4a94.js";import"./asyncToGenerator.5c39b976.js";e({target:document.querySelector("#sapper")});
