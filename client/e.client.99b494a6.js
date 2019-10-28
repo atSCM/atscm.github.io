1da3e166dc810b67e2185f8735ea5663c8b4f5ed
ls-age CI <ci@ls-age.com>
2019-10-28 06:03:40 +0000

    chore(package): Update dependency fuzzy-search to v3.0.2 (#2) [ci skip]

---
+++ client/e.client.99b494a6.js
@@ -0,0 +1,1 @@
+import"./c.SEO.1ae5729d.js";import{s as e}from"./c.app.7827b762.js";e({target:document.querySelector("#sapper")});
--- client/legacy/e.client.1cbb0faf.js
+++
@@ -1,1 +0,0 @@
-import"./c.SEO.0889f4f2.js";import{s as e}from"./c.app.a8ad9ec9.js";import"./c.asyncToGenerator.aec2823c.js";e({target:document.querySelector("#sapper")});
