c4fe6c90cc1098fda319942376ea5b3502c5a23c
ls-age CI <ci@ls-age.com>
2019-12-06 15:26:17 +0000

    chore(package): Update babel monorepo to v7.7.5 [ci skip]

---
+++ client/legacy/e.client.78aa9a68.js
@@ -0,0 +1,1 @@
+import"./c.SEO.a8c9fde0.js";import{i as e,s as a,a as r}from"./c.app.f2be35a7.js";import"./c.index.786e8391.js";e(r),a({target:document.querySelector("#sapper")});
--- client/legacy/e.client.b50cb17c.js
+++
@@ -1,1 +0,0 @@
-import"./c.SEO.c18e6e8b.js";import{i as e,s as a,a as r}from"./c.app.155fa67c.js";import"./c.index.36572018.js";e(r),a({target:document.querySelector("#sapper")});
